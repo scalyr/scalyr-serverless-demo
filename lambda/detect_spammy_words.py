70b3e91f636178ca9d0509c293436fa0c7825703
Brian Christie <bchristie@scalyr.com>
2019-11-08 00:29:35 +0100

    Refactor detect_spammy_words, begin propogating TraceIDs

--- lambda/detect_spammy_words.py
+++ lambda/detect_spammy_words.py
@@ -4,6 +4,7 @@ import os
 from lambda_common import S3Url, UpdateSpamScoreStub
 
 rekognition_client = boto3.client('rekognition')
+update_spam_score = UpdateSpamScoreStub()
 
 
 def handler(event, context):
@@ -19,46 +20,20 @@ def handler(event, context):
 
     s3_image = S3Url(message['ImageURL'])
 
-    print('bucket: {}, key: {}'.format(s3_image.bucket, s3_image.key))
+    # Detect text with Rekognition and get a list of dicts with results
+    detected_text = get_text_from_image(s3_image.bucket, s3_image.key)
+    print('rekognition response: {}'.format(detected_text))
 
-    rekognition_response = process_text(s3_image.bucket, s3_image.key)
-    print('rekognition response: {}'.format(rekognition_response))
-
-    return {
-        'statusCode': 200,
-        'headers': {'Content-Type': 'text/plain'},
-        'body': 'Hello, this is the detect_spammy_words lambda',
-    }
-
-
-def process_text(bucket: str, key: str):
-    """
-    Invokes the AWS Rekognition API with a given bucket and key, and returns a
-    spam score from 0 to 1, 1 being most likely to be spam.
-    :param bucket: An AWS S3 bucket name
-    :type bucket: str
-    :param key: A path to an S3 object, without the bucket name or a leading /
-    :type key: str
-    :return:
-    """
-    # TODO: It may make sense to have an additional abstraction between
-    # detecting the text and calculating the spam_score. Putting it all in here for now.
-
-    # TODO: Temporary hack to make MyPy stop complaining about incompatible types
+    # Get the confidence threshold to use
     __image_confidence_threshold_value = os.environ.get("IMAGE_CONFIDENCE_THRESHOLD")
     if __image_confidence_threshold_value is None:
         raise MissingSpamScoreThreshold()
     else:
         __image_confidence_threshold = float(__image_confidence_threshold_value)
 
-    update_spam_score = UpdateSpamScoreStub()
-
-    response = rekognition_client.detect_text(
-        Image={'S3Object': {'Bucket': bucket, 'Name': key}}
-    )
-
-    text_detections = response['TextDetections']
-    for text in text_detections:
+    # Process the detected text values, if any bad words are found, update the
+    # Spam score and stop processing further
+    for text in detected_text:
         print(
             f'Detected text: {text["DetectedText"]}, '
             f'Confidence: {text["Confidence"]:.2f}%, '
@@ -68,9 +43,37 @@ def process_text(bucket: str, key: str):
         if text["Confidence"] >= __image_confidence_threshold:
             if is_bad_word(text["DetectedText"]) is True:
                 update_spam_score_result = update_spam_score.invoke(
-                    {'bucket': bucket, 'key': key}, 'detect_spammy_words', 1
+                    {'bucket': s3_image.bucket, 'key': s3_image.key},
+                    'detect_spammy_words',
+                    1,
                 )
-                print(f'update_spam_score_result: {update_spam_score_result}')
+                print(f"update_spam_score_result: {update_spam_score_result}")
+                break
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'text/plain'},
+        'body': 'Hello, this is the detect_spammy_words lambda',
+    }
+
+
+def get_text_from_image(bucket: str, key: str):
+    """
+    Invokes the AWS Rekognition API with a given bucket and key, and returns a
+    a list of dicts with the detection results.
+    :param bucket: An AWS S3 bucket name
+    :type bucket: str
+    :param key: A path to an S3 object, without the bucket name or a leading /
+    :type key: str
+    :return: list
+    """
+
+    response = rekognition_client.detect_text(
+        Image={'S3Object': {'Bucket': bucket, 'Name': key}}
+    )
+
+    text = response['TextDetections']
+    return text
 
 
 def is_bad_word(word: str) -> bool:
